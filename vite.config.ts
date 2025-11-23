e81266396675188ce21fd9e5e963241696fdc92f
Thomas Benfer <thomasbenfer@gmai.com>
2025-11-23 13:34:27 +0100

    vite changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Bean_WebApp/",
+  base: process.env.NODE_ENV === "production" ? "/Bean_WebApp/" : "/",
   plugins: [react()],
 })
